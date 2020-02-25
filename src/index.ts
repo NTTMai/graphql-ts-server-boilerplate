60763088d5ab6222a0bc43b31f80c70e63a5f9de
NTPhuong <thaiphuongn1@gmail.com>
2020-02-25 16:22:16 +0700

    Setting up Jest

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,16 @@ import { GraphQLServer } from "graphql-yoga";
 import { resolvers } from "./resolvers";
 import { createConnection } from "typeorm";
 
-const server = new GraphQLServer({
-  typeDefs: "./src/schema.graphql",
-  resolvers
-});
+export const startServer = async () => {
+  const server = new GraphQLServer({
+    typeDefs: "./src/schema.graphql",
+    resolvers
+  });
 
-createConnection().then(() => {
-  server.start(() => console.log("Server is running on localhost:4000"));
-});
+  await createConnection();
+  await server.start();
+
+  console.log("Server is running on localhost:4000");
+};
+
+startServer();
